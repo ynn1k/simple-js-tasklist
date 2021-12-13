37e47d32319668c1900f365f472b8ab8c89bb7f3
Yannik Hewlik <hi@yannik.dev>
2021-12-13 17:22:59 +0100

    fix duplicate tasknames bug #2; refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -6,107 +6,98 @@ const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
 //app vars
-let tasks = [];
-let completed = [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 class Tasklist {
     static init() {
-        if(localStorage.getItem('tasks') !== null) {
-            tasks = JSON.parse(localStorage.getItem('tasks'));
-        }
+        tasks.forEach(task => Tasklist.renderTask(task));
 
-        if(localStorage.getItem('completed') !== null) {
-            completed = JSON.parse(localStorage.getItem('completed'));
-        }
-
-        tasks.forEach(function(task){
-            Tasklist.renderTask(task)
-        });
-        
-        Tasklist.filter();
+        Tasklist.filter();//TODO: ???
     }
 
+    /**
+     * reders a single task to the bottom
+     * @param {object} task
+     */
     static renderTask(task) {
-        let checked;
-        if(completed.indexOf(task) > -1){checked = "checked"}
-        const markup = `
-                <li class="d-flex list-group-item list-group-item-action task">
-                    <div class="form-check ms-1">
-                        <input type="checkbox" ${checked} class="form-check-input" id="${task}">
-                        <label class="form-check-label" for="${task}">${task}</label>
-                    </div>
-                    <a href="#" class="ms-auto delete-task">
-                        <i class="far fa-trash-alt"></i>
-                    </a>
-                </li>`;
-        const frag = document.createRange().createContextualFragment(markup.trim());
-        tasklist.appendChild(frag);
+        let checked
+
+        if(task.status === 'completed') {
+            checked = 'checked'
+        } else {
+            checked = 'pending'
+        }
+
+        const html = `
+            <li class="d-flex list-group-item list-group-item-action task" data-id="${task.date}">
+                <div class="form-check ms-1">
+                    <input type="checkbox" ${checked} class="form-check-input" id="task_${task.date}">
+                    <label class="form-check-label" for="task_${task.date}">${task.name}</label>
+                </div>
+                <a href="#" class="ms-auto delete-task">
+                    <i class="far fa-trash-alt"></i>
+                </a>
+            </li>`;
+
+        let doc = new DOMParser().parseFromString(html.trim(), 'text/html')
+        let taskNode = doc.body.querySelector('li');
+        tasklist.appendChild(taskNode);
     }
 
-    static add(e) {
-        e.preventDefault();
+    static add(event) {
+        event.preventDefault();
+        let taskName = taskInput.value.trim();
 
-        if(taskInput.value === "") {
-            alert("No task name given.");
-        } else {
-            tasks.push(taskInput.value);
-            Tasklist.renderTask(taskInput.value);
-            taskInput.value = "";
-            Tasklist.store(tasks);
+        if(taskName.length) {
+            let task = {
+                name: taskName,
+                status: 'pending',
+                date: new Date().getTime(),
+                order: '',
+            }
+
+            tasks.push(task);
+            Tasklist.renderTask(task);
+
+            form.reset();
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+
             Tasklist.filter();
         }
     }
 
-    static remove(e) {
-        if(e.target.parentElement.classList.contains('delete-task')){
-            e.preventDefault();
-            if(confirm('Delete task: ' + e.target.parentElement.parentElement.textContent.trim())) {
-                tasks.forEach(function (task, i) {
-                    if(e.target.parentElement.parentElement.textContent.trim() === task) {
-                        e.target.parentElement.parentElement.remove();
-                        tasks.splice(i, 1);
-                    }
-                });
-                Tasklist.store();
+    static remove(event) {
+        if(event.target.parentElement.classList.contains('delete-task')){
+            event.preventDefault();
+            if(confirm('Delete task: ' + event.target.parentElement.parentElement.textContent.trim())) {
+                let date = parseInt(event.target.offsetParent.dataset.id)
+                tasks = tasks.filter(task => task.date !== date)
+                event.target.offsetParent.remove()
+
+                localStorage.setItem('tasks', JSON.stringify(tasks));
                 Tasklist.filter();
             }
         }
     }
 
-    static complete(e) {
-        if(e.target.parentElement.classList.contains('form-check')){
-            if(completed.includes(e.target.parentElement.parentElement.textContent.trim())) {
-                completed.forEach(function (task, i) {
-                    if(e.target.parentElement.parentElement.textContent.trim() === task) {
-                        completed.splice(i, 1);
-                    }
-                });
-            } else {
-                completed.push(e.target.parentElement.parentElement.textContent.trim());
-            }
-            Tasklist.store();
+    static complete(event) {
+        if(event.target.parentElement.classList.contains('form-check')){
+            let date = parseInt(event.target.offsetParent.dataset.id)
+            tasks.find(task => task.date === date).status = 'completed';
+            localStorage.setItem('tasks', JSON.stringify(tasks));
         }
-    }
-
-    static store() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-        localStorage.setItem('completed', JSON.stringify(completed));
     }
 
     static deleteAll() {
         if(confirm('This will delete ALL tasks')) {
-            while (tasklist.firstChild) {
-                tasklist.removeChild(tasklist.firstChild);
-            }
+            tasklist.innerHTML = ''
             localStorage.removeItem('tasks');
-            localStorage.removeItem('completed');
-            tasks = [];
-            Tasklist.init();
+            window.location.reload()
         }
     }
 
-    static filter(e) {
-        if(tasks.length > 1) {
+    static filter(event) {
+        if(tasks.length > 2) {
             document.querySelector('.clear-tasks').style.display = 'inline-block';
             document.querySelector('.filter-wrapper').style.display = 'block';
         } else {
@@ -114,8 +105,8 @@ class Tasklist {
             document.querySelector('.filter-wrapper').style.display = 'none';
         }
 
-        if(e){
-            const text = e.target.value.toLowerCase();
+        if(event){
+            const text = event.target.value.toLowerCase();
             document.querySelectorAll(".task").forEach(function(task){
                 if(task.querySelector(".form-check-label").textContent.toLowerCase().trim().indexOf(text) !== -1) {
                     task.setAttribute( 'style', "display: flex !important");
@@ -134,9 +125,3 @@ tasklist.addEventListener("click", Tasklist.remove);
 tasklist.addEventListener("mouseup", Tasklist.complete);
 clearTasks.addEventListener("click", Tasklist.deleteAll);
 filter.addEventListener("keyup", Tasklist.filter);
-
-if (module.hot) {
-    module.hot.dispose(function () {
-        window.location.reload();
-    });
-}
