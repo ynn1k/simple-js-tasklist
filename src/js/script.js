96f77db6debd5ce028ab9672bef999d9e5088aaa
Yannik Hewlik <hi@yannik.dev>
2022-02-06 21:12:30 +0100

    naming

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,14 @@
 class Tasklist {
     constructor() {
         //UI vars
-        this.form = document.querySelector("#task-form");
+        this.newTaskform = document.querySelector("#new-task-form");
         this.tasklist = document.querySelector("#tasklist");
         this.clearTasks = document.querySelector("#clear-tasks");
         this.clearCompTasks = document.querySelector("#clear-completed-tasks");
         this.filterEl = document.querySelector("#filter");
-        this.taskInput = document.querySelector("#task");
 
         //app vars
         this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        this.deferredReorderEvent = null;
 
         this.init();
     }
@@ -20,7 +18,7 @@ class Tasklist {
         this.filterTasks();
 
         //event listeners
-        this.form.addEventListener("submit", (event) => this.add(event));
+        this.newTaskform.addEventListener("submit", (event) => this.add(event));
         this.tasklist.addEventListener("click", (event) => this.remove(event));
         this.tasklist.addEventListener("mouseup", (event) => this.complete(event));
         this.tasklist.addEventListener("drag", (event) => this.reorder(event));
@@ -36,14 +34,24 @@ class Tasklist {
      */
     renderTask(task) {
         let checked = task.status === 'completed' ? 'checked' : 'pending';
+        let dueDate = '';
+
+        if (task.dueDate) {
+            dueDate = new Date(task.dueDate);
+            let dd = String(dueDate.getDate()).padStart(2, '0');
+            let mm = String(dueDate.getMonth() + 1).padStart(2, '0');
+            let YYYY = dueDate.getFullYear();
+            dueDate = dd + '.' + mm + '.' + YYYY;
+        }
 
         const html = `
-            <li class="d-flex list-group-item list-group-item-action task" data-id="${task.date}" draggable="true">
+            <li class="d-flex list-group-item list-group-item-action align-items-center task" data-id="${task.created}" draggable="true">
                 <div class="form-check ms-2">
-                    <input type="checkbox" ${checked} class="form-check-input" id="task_${task.date}">
-                    <label class="form-check-label" for="task_${task.date}">${task.name}</label>
+                    <input type="checkbox" ${checked} class="form-check-input" id="task_${task.created}">
+                    <label class="form-check-label task-name" for="task_${task.created}">${task.name}</label>
                 </div>
-                <a href="#" class="ms-auto delete-task text-decoration-none">
+                <small class="fw-light text-secondary ms-auto">${dueDate}</small>
+                <a href="#" class="delete-task text-decoration-none ms-4">
                     <i class="far fa-trash-alt"></i>
                 </a>
                 <a href="#" class="ms-4 d-flex align-items-center text-decoration-none drag-handle">
@@ -95,21 +103,22 @@ class Tasklist {
 
     add(event) {
         event.preventDefault();
-        console.log(this.taskInput);
-        let taskName = this.taskInput.value.trim();
+
+        let taskName = this.newTaskform.querySelector('#task-name').value.trim();
+        let taskDueDate = this.newTaskform.querySelector('#task-due-date').value;
 
         if (taskName.length) {
             let task = {
                 name: taskName,
                 status: 'pending',
-                date: new Date().getTime(),
-                order: '',
+                created: new Date().getTime(),
+                dueDate: taskDueDate
             }
 
             this.tasks.push(task);
             this.renderTask(task);
 
-            this.form.reset();
+            this.newTaskform.reset();
             localStorage.setItem('tasks', JSON.stringify(this.tasks));
 
             this.filterTasks();
@@ -119,10 +128,9 @@ class Tasklist {
     remove(event) {
         if (event.target.parentElement.classList.contains('delete-task')) {
             event.preventDefault();
-            if (confirm('Delete task: ' + event.target.parentElement.parentElement.textContent.trim())) {
-                let date = parseInt(event.target.offsetParent.dataset.id)
-                console.log(this.tasks)
-                this.tasks = this.tasks.filter(task => task.date !== date)
+            if (confirm('Delete task: ' + event.target.offsetParent.querySelector('.task-name').textContent)) {
+                let created = parseInt(event.target.offsetParent.dataset.id)
+                this.tasks = this.tasks.filter(task => task.created !== created)
                 event.target.offsetParent.remove()
 
                 localStorage.setItem('tasks', JSON.stringify(this.tasks));
@@ -133,8 +141,8 @@ class Tasklist {
 
     complete(event) {
         if (event.target.parentElement.classList.contains('form-check')) {
-            let date = parseInt(event.target.offsetParent.dataset.id)
-            const task = this.tasks.find(task => task.date === date);
+            let created = parseInt(event.target.offsetParent.dataset.id)
+            const task = this.tasks.find(task => task.created === created);
             task.status = task.status === "completed" ? "pending" : "completed";
             localStorage.setItem('tasks', JSON.stringify(this.tasks));
         }
@@ -152,7 +160,7 @@ class Tasklist {
         if (confirm('This will delete ALL COMPLETED tasks')) {
             this.tasks.forEach(task => {
                 if (task.status === 'completed')
-                    document.querySelector(`[data-id="${task.date}"]`).remove();
+                    document.querySelector(`[data-id="${task.created}"]`).remove();
             });
             this.tasks = this.tasks.filter(task => task.status !== 'completed');
             localStorage.setItem('tasks', JSON.stringify(this.tasks));
